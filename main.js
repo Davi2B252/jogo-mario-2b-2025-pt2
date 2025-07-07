5429f218a9549f475eb29fb3b86be3b47d172f8d
Davi2B252 <davi.souza.soares@escola.pr.gov.br>
2025-07-07 18:46:36 -0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const mario = document.querySelector(".mario")
 const pipe = document.querySelector(".pipe")
+const nuvem = document.querySelector(".nuvem")
+const startButton = document.querySelector(".startButton")
+const gameOverScreen = document.querySelector(".gameOverScreen")
+const scoreElement = document.querySelector(".scoreElement")
+
+audioStart = new Audio("./sound/");
+const gameOverSound = new Audio("./sound/");
+
+let gameStarted = false;
+let score = 0;
+
+const startGame = () => {
+    gameStarted = true;
+    audioStart.play();
+
+    pipe.style.animation = "pipe-animation 1.5s infinite linear";
+
+    startButton.style.display = "none";
+    mario.style.opacity = "1";
+    pipe.style.opacity = "1";
+    nuvem.style.opacity = "1";
+}
+
 
 const jump = () => {
     mario.classList.add("jump");
@@ -8,6 +31,13 @@ const jump = () => {
         mario.classList.remove("jump");
     }
     ,500);
+}
+
+const updateScore = () => {
+    score += 1;
+    scoreElement.textContent = score;
+    const animationSpeed = 1.5/ (1 + score/500);
+    pipe.style.animation = 'pipe-animation $(animationSpeed)s infinite linear';
 }
 
 const loop = setInterval(() => {
@@ -21,11 +51,23 @@ const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
         mario.style.animation = "none";
         mario.style.left = `${marioPosition}px`;
 
-        mario.src = "./imagem/"game-over.png";
+        mario.src = "./imagem/game-over.png";
         mario.style.width = "75px";
         mario.style.marginLeft = "50px";
 
+        audioStart.pause();
+        gameOverSound.play();
+
         clearInterval(loop);
+        gameOverScreen.style.display = "flex";
+    } else if (pipePosition < 0 && gameStarted) {
+        updateScore();
+        pipe.style.left = '';
+    }
     }
 },10);
 document.addEventListener("keydown", jump);
+
+const restartGame = () => {
+    window.location.reload();
+}
